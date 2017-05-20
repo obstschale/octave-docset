974dbc655638c313f7553a755a4b055bc95670f6
Lilian Besson <Naereen@users.noreply.github.com>
2017-05-20 19:40:59 +0200

    Add sheband and comments

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 VERSION=v4.2.1
 DOCS_URL=https://www.gnu.org/software/octave/doc/$VERSION/
+echo -e "Starting to build octave.docset for version $VERSION"
 
 # clean up previous remains, if any
 rm -rf Contents/Resources
@@ -8,6 +10,7 @@ mkdir -p Contents/Resources
 
 # fetch the whole doc site
 cd Contents/Resources
+echo -e "Starting to download Octave $VERSION documentation from $DOCS_URL"
 wget -m -p -E -k -np $DOCS_URL
 
 # change folder name to just Documents
@@ -16,6 +19,7 @@ rm -rf www.gnu.org
 cd ../../
 
 # bundle up!
+echo -e "Building the Octave.docset folder..."
 mkdir Octave.docset
 cp -r Contents Octave.docset
 cp assets/icon* Octave.docset
@@ -24,4 +28,5 @@ cp assets/icon* Octave.docset
 python octdoc2set.py
 
 # Create gzip bundle for Dash Contribution
+echo -e "Archiving to Octave.tgz ..."
 tar --exclude='.DS_Store' -cvzf Octave.tgz Octave.docset
